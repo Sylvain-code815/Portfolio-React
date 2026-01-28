2b52a303689f5fb3d53dfa1b111159911f7c8daa
Sylvain <sylvain.laurent123@sfr.fr>
2026-01-28 15:16:38 +0100

    First commit

---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+export const projectsData = [
+    {
+        id: 1,
+        title: "The resto"
+        description: "Site de restauration attractif avec un css poussé"
+        image: "/img/resto.png",
+        tags: ["HTML", "CSS"]
+        link: "https://resto.png"
+    }
+    {
+        id: 2,
+        title:
+        description:
+        image:
+        tags:
+        link:
+    }
+    ...
+]
